d9580bdf3ad9a0e6f61ac5132c2361a43d072e1f
ShanbhagKrupa <shanbhagkrupa@gmail.com>
2024-11-29 18:42:23 +0530

    Assignment# 10
    Print 1 to 10 using For Loop

---
+++ A10_ForLoop_1to10.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class A10_ForLoop_1to10 
+{
+
+	public static void main(String[] args)
+	{
+		for(int i = 1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
